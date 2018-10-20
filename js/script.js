53697a748b3e046520fccc4c1c74abff60a3dfdc
Irina <popova_ira210398@mail.ru>
2018-10-20 23:55:43 +0600

    added Timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
    'use strict';
 
+   // Tabs
    workTabs('.info-header-tab', '.info-header', '.info-tabcontent');
 
    function workTabs (tabSelector, tabWrapSelector, tabContentSelector) {
@@ -37,4 +38,58 @@ document.addEventListener('DOMContentLoaded', function() {
            }
        }
    }
+
+   // Timer
+   let deadline = '2018-10-23';
+
+   function getTimeRemaining(endTime) {
+       let t = Date.parse(endTime) - Date.parse(new Date()),
+           seconds = Math.floor((t/1000) % 60),
+           minutes = Math.floor((t/1000/60) % 60),
+           hours = Math.floor((t/1000/60/60));
+       if (hours.toString().length == 1) { // Это как-то можно оптимизировать,
+           hours = '0' + hours;            // чтоб не писать 3 условия?
+       }
+       if (minutes.toString().length == 1) {
+           minutes = '0' + minutes;
+       }
+       if (seconds.toString().length == 1) {
+           seconds = '0' + seconds;
+       }
+       return {
+         'total' : t,
+         'hours' : hours,
+         'minutes' : minutes,
+         'seconds' : seconds
+       };
+   }
+
+   setClock('timer', deadline);
+
+   function setClock(id, endTime) {
+       let timer = document.getElementById(id),
+           hours = timer.querySelector('.hours'),
+           minutes = timer.querySelector('.minutes'),
+           seconds = timer.querySelector('.seconds'),
+           timeInterval = setInterval(updateClock, 1000);
+
+       function updateClock() {
+           let t = getTimeRemaining(endTime);
+           hours.textContent = t.hours;
+           minutes.textContent = t.minutes;
+           seconds.textContent = t.seconds;
+
+           if (t.total <= 0) {
+               clearInterval(timeInterval);
+               hours.textContent = '00';
+               minutes.textContent = '00';
+               seconds.textContent = '00';
+           }
+       }
+   }
+
+   // Scroll
+
+
+
 });
