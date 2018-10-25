7cd81eddc80f50d644c5e737eb6a9ef8970104cf
Irina <popova_ira210398@mail.ru>
2018-10-25 22:52:30 +0600

    added sentForm

--- js/script.js
+++ js/script.js
@@ -130,4 +130,69 @@ document.addEventListener('DOMContentLoaded', function() {
        });
    });
 
+   // Form
+
+    let message = {
+      loading: 'Загрузка...',
+      success: 'Спасибо, с вами скоро свяжутся!',
+      failure: 'Что-то пошло не так'
+    };
+
+    let formModal = document.querySelector('.main-form'),
+        formContact = document.getElementById('form'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    sendForm(formModal);
+    sendForm(formContact);
+
+    function sendForm (form) {
+        let input = form.querySelectorAll('input');
+        // input.forEach(function(item, key) {
+        //     item.addEventListener('keydown', function(e) {
+        //         console.log(e.keyCode);
+        //         if (e.keyCode != 187 || e.keyCode < 48 || e.keyCode > 57) {
+        //             e.preventDefault();
+        //         }
+        //     });
+        // });
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            form.appendChild(statusMessage);
+            console.log(input);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+
+            let json = JSON.stringify(obj);
+            request.send(json);
+
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4 && request.status === 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
+    }
+
+
+
 });
